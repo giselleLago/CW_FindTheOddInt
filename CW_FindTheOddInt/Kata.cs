e42a098f1225ebde923aaf9e2214cf863d2813eb
giselleLago <gisellelago25994@gmail.com>
2020-08-05 11:13:31 +0200

    Add project files.

---
+++ CW_FindTheOddInt/Kata.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CW_FindTheOddInt
+{
+    public class Kata
+    {
+        public int find_it(int[] seq)
+        {
+            return seq.GroupBy(g => g).Single(x => x.Count() % 2 != 0).Key;
+            /*var countOfItems = new Dictionary<int, int>();
+            foreach (int eachNumber in seq)
+            {
+                
+                if (countOfItems.ContainsKey(eachNumber))
+                {
+                    countOfItems[eachNumber]++;
+                }
+                else
+                {
+                    countOfItems.Add(eachNumber, 1);
+                }
+                    
+            }
+            foreach (var item in countOfItems)
+            {
+                if (item.Value % 2 != 0)
+                {
+                    return item.Key;
+                }
+            }
+            return -1;*/
+        }
+    }
+}
---
+++ CW_FindTheOddInt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CW_FindTheOddInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = new Kata();
+            Console.WriteLine(a.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+            Console.ReadKey();
+        }
+    }
+}
